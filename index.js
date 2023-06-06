54fea2d1874bb3e01a76672ab6fcb13ef163aadf
yusuke-nishida-88 <134067398+yusuke-nishida-88@users.noreply.github.com>
2023-06-06 17:51:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-window.onload = function() {
-  const defaultLiffId = "1661178769-QELpevEK";
-  initializeLiff(defaultLiffId);
-};
-
-function initializeLiff(defaultLiffId) {
-  liff
-  .init({
-      liffId: defaultLiffId
-  })
-  .then(() => {
-      liff.scanCodeV2().then(result => {
-          const stringifiedResult = result.value;
-          liff.sendMessages([{
-              'type': 'text',
-              'text': stringifiedResult
-          }]).then(() => {
-              liff.closeWindow();
-          }).catch((error) => {
-              window.alert('Error sending message: ' + error);
-          });
-      }).catch(err => {
-          window.alert('scanCode failed!');
-      });
-  })
-  .catch((err) => {
-      window.alert('Something went wrong with LIFF initialization.');
-  });
-}
+liff
+.init({
+    liffId: "1661178769-QELpevEK"
+})
+.then(() => {
+    liff.scanCodeV2().then(result => {
+        const stringifiedResult = result.value;
+        liff.sendMessages([{
+            'type': 'text',
+            'text': stringifiedResult
+        }]).then(() => {
+            liff.closeWindow();
+        }).catch((error) => {
+            window.alert('Error sending message: ' + error);
+        });
+    }).catch(err => {
+        window.alert('scanCode failed!');
+    });
+})
+.catch((err) => {
+    window.alert('Something went wrong with LIFF initialization.');
+});

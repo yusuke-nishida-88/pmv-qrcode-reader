720770839b5390ccd7c85cad2dd3241e95387bc1
yusuke-nishida-88 <134067398+yusuke-nishida-88@users.noreply.github.com>
2023-08-04 17:37:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ window.onload = function() {
         liffId: "1661178769-pwmMGL6x"
     })
     .then(() => {
-        if (liff.isLoggedIn()) {
         const idToken = liff.getDecodedIDToken();
         console.log(idToken);
-        }
     }).catch((err) => {
         console.log('error')
     });

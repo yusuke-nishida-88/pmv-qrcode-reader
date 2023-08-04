7dae44e72f63999a9c8c7ccf538089f1652290cc
yusuke-nishida-88 <134067398+yusuke-nishida-88@users.noreply.github.com>
2023-08-04 18:03:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ window.onload = function() {
         liffId: "1661178769-pwmMGL6x"
     })
     .then(() => {
-        const idToken = liff.getDecodedIDToken();
-        window.alert(idToken.birthdate);
-    }).catch((err) => {
-        window.alert('error')
+        liff.scanCodeV2().then(result => {
+            const stringifiedResult = result.value;
+            liff.sendMessages([{
+                'type': 'text',
+                'text': stringifiedResult
+            }]).then(() => {
+                liff.closeWindow();
+            }).catch((error) => {
+                window.alert('Error sending message: ' + error);
+            });
+        }).catch(err => {
+            window.alert('scanCode failed!');
+        });
+    })
+    .catch((err) => {
+        window.alert('Something went wrong with LIFF initialization.');
     });
   }
